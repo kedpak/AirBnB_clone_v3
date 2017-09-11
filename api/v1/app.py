fc8a4bb35fbe4ebbebfca2f807b4755fcfb100ff
cnov20 <128@holbertonschool.com>
2017-09-11 21:10:39 +0000

    Implemented 404 not found error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 from models import storage
+from flask import jsonify
 from api.v1.views import app_views
 import os
 
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    return (jsonify({"error": "Not found"}), 404)
 
 @app.teardown_appcontext
 def route_close(exception):
