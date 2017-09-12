03ec877f4179837fcac3accee14b34b100b0743f
cnov20 <128@holbertonschool.com>
2017-09-12 07:30:46 +0000

    Implemented strict slash handling in app.py file, adjusted datetime handling in basemodel.py and implemented state routes for GET/GET<id>/DELETE

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,12 +10,13 @@ from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return (jsonify({"error": "Not found"}), 404)
+    return (jsonify({"error": "Not Found"}), 404)
 
 
 @app.teardown_appcontext
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
 """ This module handles all default RESTful API actions """
-from flask import request, jsonify
+from flask import abort, jsonify, request
 from models import storage
+from models.base_model import *
 from models.state import State
+from models.engine.db_storage import DBStorage
 from api.v1.views import app_views
 
+@app_views.route('/states', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+def states(state_id=None):
 
-@app_views.route('/states', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def states():
     new_list = []
-    if request.method == 'GET':
-        for i, j in storage.all("State").items():
-            j.to_json()
-            new_list.append(j)
-        return (j)
+    empty_dict = {}
+    states = storage.all("State").items()
+
+    for key, value in states:
+        if request.method == 'GET':
+            if state_id is not None:
+                if state_id == value.id:
+                    json_val = value.to_json()
+                    return (jsonify(json_val))
+                else:
+                    abort(404)
+
+            else:
+                json_val = value.to_json()
+                new_list.append(json_val)
+
+        if request.method == 'DELETE':
+            if state_id is not None:
+                if state_id == value.id:
+                    storage.delete(value)
+                    storage.save()
+                    return (jsonify(empty_dict))
+                else:
+                    abort(404)
+
+    return (jsonify(new_list))
