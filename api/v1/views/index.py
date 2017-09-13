743204a85ed312baea7abe695cd3b33675bc035b
kedpak <kedpak@yahoo.com>
2017-09-13 02:55:55 +0000

    add method to return json of classes with count as values

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,24 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models  import storage
+
 
 @app_views.route('/status')
 def status():
     return (jsonify({"status": "OK"}))
+
+@app_views.route('/stats')
+def count():
+    cls = ["Amenity", "City", "Place", "Review", "State", "User"]
+    count = []
+    for i in cls:
+        objects = storage.count(i)
+        count.append(objects)
+    cls_count = jsonify({"amenities": objects, 
+            "cities": count[0],
+            "places": count[1],
+            "reviews": count[2],
+            "states": count[3],
+            "users": count[4]
+            })
+    return cls_count
