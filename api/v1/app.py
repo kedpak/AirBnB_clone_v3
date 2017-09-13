d9700b36d4b344dec1e3b646c1ab6ea4dd2d6b2e
kedpak <kedpak@yahoo.com>
2017-09-13 02:08:46 +0000

    add module guard inside app.py, jsonify status code and return in index, alter order of imports inside init file to fix circular dependency

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
-from models import storage
-
 app = Flask(__name__)
+from models import storage
+from api.v1.views import app_views
+import os
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def route_close(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('HBNB_API_HOST'), port=os.environ.get('HBNB_API_PORT'))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 from api.v1.views import app_views
+from flask import jsonify
 
 @app_views.route('/status')
 def status():
-    return
+    return (jsonify({"status": "OK"}))
