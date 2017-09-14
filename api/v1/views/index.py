ce7e98780edcda70c5f9f0e0a66528d337f4ef0e
kedpak <kedpak@yahoo.com>
2017-09-14 01:26:35 +0000

    add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+create a route /status on the object app_views
+that returns a JSON
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
